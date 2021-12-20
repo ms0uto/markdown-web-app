ab124fe04056c14a049488d777dc7d4138c1a0ab
Goblin <39985547+ms0uto@users.noreply.github.com>
2021-12-20 23:10:19 +0100

    JS code split

---
+++ app.js
@@ -0,0 +1,49 @@
+const defaultText = `Example Heading
+=======
+Sub-heading
+-----------
+
+## Code block
+
+\`\`\`JavaScript
+function add(a, b){
+   return a + b;
+}
+add(3, 4);
+\`\`\`
+
+## Links
+<a href="https://github.com/ms0uto" target="_blank">Personal github page (new tab)</a>
+
+[Google home page (same tab)](https://google.com/)
+
+## Images
+
+![Code](https://blog.codepen.io/wp-content/uploads/2012/06/Button-Fill-Black-Small.png)
+`
+function download() {
+  var text = document.getElementById("text-area").value;
+  var blob = new Blob([text], { type: "text/markdown" });
+  var anchor = document.createElement("a");
+  date = Date.now();
+  anchor.download = `markdown-${date}.md`;
+  anchor.href = window.URL.createObjectURL(blob);
+  anchor.target = "_blank";
+  anchor.style.display = "none"; // just to be safe!
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
+}
+function open() {
+  inputfile.click();
+  document.getElementById("inputfile").addEventListener("change", function () {
+    var fr = new FileReader();
+    fr.onload = function () {
+      var textarea = document.getElementById("text-area");
+      textarea.value = fr.result;
+      document.getElementById("markdown").innerHTML = marked.parse(fr.result);
+      textarea.focus();
+    };
+    fr.readAsText(this.files[0]);
+  });
+}
